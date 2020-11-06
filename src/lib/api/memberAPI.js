49c7ad83da41f6e3718e7747ae48b5236023169a
JunyupHong <junyup.hong@gmail.com>
2020-11-06 19:11:55 +0900

    url 수정 & README.md 작성

--- src/lib/api/memberAPI.js
+++ src/lib/api/memberAPI.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://127.0.0.1:5000/api/members';
+// const url = 'http://127.0.0.1:5000/api/members';
+const url = 'http://ec2-13-124-127-8.ap-northeast-2.compute.amazonaws.com:3000/api/members';
 
 const getMembersAPI = async () => {
     try {
