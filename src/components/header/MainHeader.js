de38a6b11068b6a3dab37a8e195d86ed6a215442
JunyupHong <junyup.hong@gmail.com>
2020-11-06 13:45:02 +0900

    [STEP 3] MainHeader 만들기
    	* MainHeader Component
    		flex 속성을 이용한 반응형
    		local image 파일 적용하기
    
    	* Button Component
    		Route의 history 속성을 이용해서 페이지 전환

--- src/components/header/MainHeader.js
+++ src/components/header/MainHeader.js
@@ -1,5 +1,26 @@
-function MainHeader() {
-    return (<header>여기는 header입니다</header>);
+import './MainHeader.scss';
+import MenuIcon from '../../assets/icons/mdi_dehaze.svg';
+import Button from '../button/Button';
+
+function MainHeader({ history }) {
+    // console.log(history);
+    return (
+        <header className="main-header">
+            <img className="main-header-icon" src={MenuIcon} alt="menu icon"/>
+            <nav className="main-header__nav">
+                <Button text="[ON SOPT] Web Part" onClickFunc={() => history.push('/')}></Button>
+                <span> / </span>
+                <Button text="파트원 소개" onClickFunc={() => history.push('/members')}></Button>
+            </nav>
+            <div className="empty"></div>
+            <div className="main-header__nav">
+                <Button text="Share"></Button>
+                <Button text="Updates"></Button>
+                <Button text="Favorites"></Button>
+                <Button text="…"></Button>
+            </div>
+        </header>
+    );
 }
 
 
